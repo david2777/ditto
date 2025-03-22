00681d877dcc7ed4e871ebdcd700506db6eb0577
David Lee-DuVoisin <daduvo11@gmail.com>
2025-03-21 18:56:25 -0700

    = Deduplicate code

--- src/ditto/main.py
+++ src/ditto/main.py
@@ -10,6 +10,16 @@ notion_db = database.NotionDatabaseManager(secrets.NOTION_DATABASE_ID)
 app = FastAPI()
 
 
+async def _process_quote(quote_item: database.NotionQuote):
+    await quote_item.process_image()
+
+    response = FileResponse(quote_item.image_path_processed.as_posix(), media_type="image/jpeg")
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
+
 @app.get("/")
 async def root_endpoint(request: Request):
     logger.info(f"request: {request.method} {request.url}")
@@ -23,14 +33,9 @@ async def next_endpoint(request: Request):
     t = Timer()
     logger.info(f"request: {request.method} {request.url}")
 
-    item = await notion_db.get_next_item()
-    await item.process_image()
-
-    logger.info(f'response: "{item.image_path_processed.as_posix()}" generated in {t.get_elapsed_time()} seconds')
-    response = FileResponse(item.image_path_processed.as_posix(), media_type="image/jpeg")
-    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
+    quote_item = await notion_db.get_next_item()
+    response = await _process_quote(quote_item)
+    logger.info(f'response: "{quote_item.image_path_processed.as_posix()}" generated in {t.get_elapsed_time()} seconds')
     return response
 
 
@@ -39,12 +44,7 @@ async def previous_endpoint(request: Request):
     t = Timer()
     logger.info(f"request: {request.method} {request.url}")
 
-    item = await notion_db.get_previous_item()
-    await item.process_image()
-
-    logger.info(f'response: "{item.image_path_processed.as_posix()}" generated in {t.get_elapsed_time()} seconds')
-    response = FileResponse(item.image_path_processed.as_posix(), media_type="image/jpeg")
-    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
+    quote_item = await notion_db.get_previous_item()
+    response = await _process_quote(quote_item)
+    logger.info(f'response: "{quote_item.image_path_processed.as_posix()}" generated in {t.get_elapsed_time()} seconds')
     return response
